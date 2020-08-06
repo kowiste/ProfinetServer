a303cbc356b16f674630867cca95b80134067b07
pablo.vivo <pablo.vivo@integrationworks.co.th>
2020-08-06 18:38:17 +0700

    [add] implement telegram

--- server.go
+++ server.go
@@ -1,6 +1,8 @@
 package profinet
 
 import (
+	"encoding/binary"
+	"fmt"
 	"io"
 	"log"
 	"net"
@@ -8,7 +10,10 @@ import (
 )
 
 const (
-	tcpMaxLength = 2084
+	tcpMaxLength     = 2084
+	isoHSize         = 7
+	minPduSize       = 16
+	pduSizeRequested = 480
 )
 
 //Server structure for the server
@@ -31,15 +36,16 @@ func (s Server) Listen(IP string) error {
 		log.Printf("Failed to Listen: %v\n", err)
 		return err
 	}
-	log.Println("Server Active on: ", IP)
+	log.Println("Profinet Server Active on: ", IP)
 	s.listeners = append(s.listeners, listen)
 	go s.accept(listen)
 	return err
 }
+
 func (s *Server) accept(listen net.Listener) error {
 	var err error
 	for {
-		s.conn, err = listen.Accept()
+		s.conn, err = listen.Accept() //Wait for a connection
 		log.Println("Active conection")
 		if err != nil {
 			if strings.Contains(err.Error(), "use of closed network connection") {
@@ -51,28 +57,29 @@ func (s *Server) accept(listen net.Listener) error {
 		if s.onConnection != nil {
 			go s.onConnection(s.conn.RemoteAddr())
 		}
-		go s.handler()
+		go s.handler() //Handler the connection
 	}
 }
 
 func (s Server) handler() {
 	defer s.conn.Close()
-	packet := make([]byte, tcpMaxLength)
 	for {
 		//log.Println("Wait command")
-		nbytes, err := s.conn.Read(packet)
+		//nbytes, err := s.conn.Read(recv)
+		recv, err := s.readConn()
 		if err != nil {
 			if err != io.EOF {
 				log.Printf("Read error %v\n", err)
 			}
 			return
 		}
-		log.Println(packet[:nbytes])
-		if _, err = s.conn.Write(createbytes(packet)); err != nil {
+		send, _ := getTelegram(recv) //createbytes(rcv)
+
+		if _, err = s.conn.Write(send); err != nil {
 			log.Println("Error writing")
 			return
 		}
-		//log.Println("SEND PACKET")
+		log.Println("SEND PACKET:  "+ fmt.Sprint(send))
 	}
 }
 
@@ -90,7 +97,7 @@ func createbytes(r []byte) []byte {
 	if r[5] == 240 && r[11] != 5 {
 		out[3] = 27
 		out = append(out, 0, 0)
-		out[25] = 0
+		out[25] = 1
 		out[26] = 20
 		log.Println("Send negotation ack")
 	} else if r[11] == 5 {
@@ -102,4 +109,43 @@ func createbytes(r []byte) []byte {
 		log.Println("Send connection ack")
 	}
 	return out
+}
+
+func (s *Server) readConn() (response []byte, err error) {
+	done := false
+	data := make([]byte, tcpMaxLength)
+	length := 0
+	for !done && err == nil {
+		// Get TPKT (4 bytes)
+		if _, err = io.ReadFull(s.conn, data[:4]); err != nil {
+			log.Printf("%T %+v", err, err)
+			return nil, err
+		}
+		// Read length, ignore transaction & protocol id (4 bytes)
+		length = int(binary.BigEndian.Uint16(data[2:]))
+		if length == isoHSize {
+			_, err = io.ReadFull(s.conn, data[4:7])
+			if err != nil { // Skip remaining 3 bytes and Done is still false
+				return nil, err
+			}
+		} else {
+			if length > pduSizeRequested+isoHSize || length < minPduSize {
+				err = fmt.Errorf("s7: invalid pdu")
+				return nil, err
+			}
+			done = true
+		}
+	}
+	// Skip remaining 3 COTP bytes
+	_, err = io.ReadFull(s.conn, data[4:7])
+	if err != nil {
+		return nil, err
+	}
+	// Receives the S7 Payload
+	_, err = io.ReadFull(s.conn, data[7:length])
+	if err != nil {
+		return nil, err
+	}
+	response = data[0:length]
+	return
 }
--- telegram.go
+++ telegram.go
@@ -1,22 +1,112 @@
 package profinet
 
-func getTelegram( rq []byte) ([]byte, error) {
+import (
+	"fmt"
+	"log"
+)
+
+func getTelegram(rq []byte) ([]byte, error) {
+	//[3 0 0 31 2 240 128 50 1 0 0 5 0 0 14 0 0 4 1 18 10 16 2 0 2 1 1 132 0 0 80]
 	out := make([]byte, 0)
 	//TPKT HEADER RFC 1006
-	out = append(out, 0x03, 0x00) //b0-1		//pvrsn (always 3), Reserved
-	out = append(out, 0x00, 0x00) //b2-3		//packet lenght [High,Low]
+	out = append(out, 0x03, 0x00)  //b0-1		//pvrsn (always 3), Reserved
+	out = append(out, 0x00, rq[3]) //b2-3		//packet lenght [High,Low]
 	//ISO-COTP
-	out = append(out, 0x02, 0xF0, 0x80) //b2-4	// seen to be always 2,240,128
+	out = append(out, 0x02, 0xF0, 0x80) //b4-6	// seen to be always 2,240,128
+	if rq[4] == 17 && rq[5] == 224 {    //ISO CONNECTION REQUEST
+		log.Println("ISO REQUEST:  " + fmt.Sprint(rq))
+		//log.Println(rq)
+		for i := 0; i < int(rq[3]-3); i++ {
+			out = append(out, 0)
+		}
+		out[3] = rq[3]
+		out[5] = 0xD0
+		return out, nil
+	}
 	//S7PDU
 	//HEADER (10-12 bytes)
-	out = append(out, 0x32)           //b5		//Protocol id (Always 0x32:50)
-	out = append(out, 0x01)           //b6		//Message type 0x01(job request), 0x02 (Ack), 0x03 (Ack Data), 0x07 (UserData)
-	out = append(out, 0x00, 0x00)     //b7-8	//Reserved
-	out = append(out, rq[11], rq[12]) //b9-10	//PDU Reference  PLC just copy it to the reply
-	out = append(out, 0x00, 0x00)     //b11-12	//Parameter Length
-	out = append(out, 0x00, 0x00)     //b13-14	//Data Length
-	out = append(out, 0x00)           //b15		//Error Class 	[optional only present in the Ack-Data message]
-	out = append(out, 0x00)           //b16		//Error Code	[optional only present in the Ack-Data message]
+	out = append(out, 0x32)           //b7		//Protocol id (Always 0x32:50)
+	out = append(out, 0x01)           //b8		//Message type 0x01(job request), 0x02 (Ack), 0x03 (Ack Data), 0x07 (UserData)
+	out = append(out, 0x00, 0x00)     //b9-10	//Reserved
+	out = append(out, rq[11], rq[12]) //b11-12	//PDU Reference  PLC just copy it to the reply
+	out = append(out, 0x00, 0x00)     //b13-14	//Parameter Length : quantity of bytes after the data lenght
+	out = append(out, 0x00, 0x00)     //b15-16	//Data Length
+
+	switch rq[17] {
+
+	case 0xF0: //Negogiation PDU
+		log.Println("NEGOTIATION:  " + fmt.Sprint(rq))
+		out[3] = 27
+		out = append(out, 0xF0)           //b17
+		out = append(out, 0x00)           //b18
+		out = append(out, 0x00, 0x00)     //b19-20
+		out = append(out, 0xFF)           //b21
+		out = append(out, 0x00)           //b22
+		out = append(out, 0x00, 0x00)     //b23-24
+		out = append(out, rq[23], rq[24]) //b25-26
+
+	default:
+
+		if out[6] == 0x02 {
+			out = append(out, 0x00) //b15			//Error Class 	[optional only present in the Ack-Data message]
+			out = append(out, 0x00) //b16			//Error Code	[optional only present in the Ack-Data message]
+		}
+		out = append(out, 0x00)             //b15
+		out = append(out, 0x00)             //b16
+		out = append(out, 0x12)             //b17				//Specification type for const 18 for read/write
+		out = append(out, 0x00)             //b18				// Lenght rest of byte
+		out = append(out, 0x10)             //b19				//Syntax ID const 16 for any type addr
+		out = append(out, 0x00)             //b20				//Variable type see tble below
+		out = append(out, 0x00)             //b21-22			//Count
+		out = append(out, 0x00)             //b25-26 			//DB number
+		out = append(out, 0x00)             //b27 				//Area ee the table below
+		out = append(out, 0x00, 0x00, 0x00) //b28-29-30 		//Addres
+	}
 
 	return out, nil
 }
+
+/*
+Variable tye
+============
+0x01 - BIT
+0x02 - BYT
+0x03 - CHAR
+0x04 - WORD
+0x05 - INT
+0x06 - DWOR
+0x07 - DIN
+0x08 - REAL
+0x09 - DATE
+0x0A - TOD
+0x0B - TIME
+0x0C - S5TIM
+0x0F - DATE AND IME
+0x1C - COUNTER
+0x1D - TIMER
+x1E - IEC TIMER
+x1F - IEC COUNTER
+x20 - HS COUNTER
+
+
+
+
+Area table
+==========
+0x03 - System info of S200 family
+0x05 - System flags ofS200 family
+0x06 - Analog inputsof S200 family
+0x07 - Analog outputs of S200 faily
+0x1C - S7 counters (C)
+0x1D - S7 timers (T)
+0x1E - IEC countes (200 family)
+0x1F - IEC timers 200 family)
+0x80 - Direct peripheral ccess (P)
+0x81 - Inputs (I)
+0x82 - Outputs (Q)
+0x83 - Flags (M) (Merer)
+0x84 - Data blocks (DB
+0x5 - Instance data blocks (DI)
+0x86 - Local data (L)
+0x87 - Unknown yet (V)
+*/
